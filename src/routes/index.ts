69d0ea326c8f5c2e931aa73b6fa3018c3a1ef6d4
Ben Whitfield <bwhitfield82@gmail.com>
2025-07-30 07:54:15 +0100

    remove route comments

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,14 +8,6 @@ import transactions from './transactions';
 const router = express.Router();
 
 export default (): express.Router => {
-  // router.get('/', (req, res) => {
-  //   res.json({ message: 'Welcome to the REST API Bank!' });
-  // });
-
-  
-  // router.use((req, res) => {
-  //   res.status(404).json({ message: 'Route not found' });
-  // });
   authentication(router);
   users(router);
   accounts(router);
